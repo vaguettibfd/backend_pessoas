614a8d18f3d3ea565083c930dfc2dab11eda339e
vaguettibfd <vaguetti.bfd@gmail.com>
2025-11-08 18:26:31 -0300

    atualização no mongoose connection

--- src/database/connect.js
+++ src/database/connect.js
@@ -6,7 +6,9 @@ dotenv.config();
 export async function connectDB() {
   try {
     console.log('🔍 URI do Mongo:', process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000 } );
     console.log('✅ MongoDB conectado com sucesso!');
   } catch (err) {
     console.error('❌ Erro ao conectar ao MongoDB:', err.message);
