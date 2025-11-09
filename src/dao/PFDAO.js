2606a058c3311933b5d3f152793d0342423d3e4a
vaguettibfd <vaguetti.bfd@gmail.com>
2025-11-09 07:59:30 -0300

    atualização do PFDAO

--- src/dao/PFDAO.js
+++ src/dao/PFDAO.js
@@ -9,33 +9,6 @@ export default class PFDAO {
   }
 
   async salvar(pfData) {
-
-      // 🔹 Cria Endereco se veio como objeto
-      if (pfData.endereco && typeof pfData.endereco === "object") {
-        const novoEnd = await Endereco.create(pfData.endereco);
-        pfData.endereco = novoEnd._id;
-      }
-  
-      // 🔹 Cria Telefones se vieram como objetos
-      if (pfData.telefones && Array.isArray(pfData.telefones)) {
-        const telIds = [];
-        for (const tel of pfData.telefones) {
-          if (typeof tel === "object") {
-            const novoTel = await Telefone.create(tel);
-            telIds.push(novoTel._id);
-          } else {
-            telIds.push(tel);
-          }
-        }
-        pfData.telefones = telIds;
-      }
-  
-      // 🔹 Cria Titulo se veio como objeto
-      if (pfData.titulo && typeof pfData.titulo === "object") {
-        const novoTitulo = await Titulo.create(pfData.titulo);
-        pfData.titulo = novoTitulo._id;
-      }
-      
     const pf = new PF(pfData);
     return await pf.save();
   }
