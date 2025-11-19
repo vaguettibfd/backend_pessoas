68b223b6d672f70093b215f32ee0d9578eafa9b6
vaguettibfd <vaguetti.bfd@gmail.com>
2025-11-19 05:27:58 -0300

    adiçao do campo data em Pessoa.js

--- src/models/Pessoa.js
+++ src/models/Pessoa.js
@@ -4,7 +4,8 @@ const PessoaSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   email: String,
   endereco: { type: mongoose.Schema.Types.ObjectId, ref: "Endereco" },
-  telefones: [{ type: mongoose.Schema.Types.ObjectId, ref: "Telefone" }]
+  telefones: [{ type: mongoose.Schema.Types.ObjectId, ref: "Telefone" }],
+  data: { type: Date }  // <-- NOVO CAMPO
 }, { discriminatorKey: "tipo", timestamps: true });
 
 export default mongoose.model("Pessoa", PessoaSchema);
